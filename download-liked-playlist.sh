0341836ee98886cbce03014c7009370bef25dea5
Mihail Zdravkov <mihail0zdravkov@gmail.com>
2018-06-15 11:43:35 +0300

    fix download-liked-playlist.sh

--- download-liked-playlist.sh
+++ download-liked-playlist.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-help="Usage:\n\tPass the link to the youtube video with playlist as the first argument of the script.\n\tThe second argument should be the range that will be downloaded (e.g. 1-42)\n\tAny additional flags can be passed after that."
+help="Usage:\n\tPass the link to the youtube video with playlist as the first argument of the script (in quotes).\n\tThe second argument should be the range that will be downloaded (e.g. \"1-42\")\n\n\tExample:\n\t\t ./download-liked-playlist.sh \"https://www.youtube.com/watch?v=i3UfHUZ-2mE&list=LLK_AJ-uea4vBS3mVjHnVHXQ\" \"1-13\""
 
 
 if [ $# -eq 0 ]
@@ -10,5 +10,5 @@ elif [ $1 == "--help" ]
 then
   echo -e $help
 else
-  youtube-dl --yes-playlist --extract-audio --audio-format "mp3" -o '%(title)s.%(ext)s' --playlist-items 1-14 --embed-thumbnail $1 $2
+  youtube-dl --yes-playlist --extract-audio --audio-format "mp3" -o '%(title)s.%(ext)s' --playlist-items $2 --embed-thumbnail $1
 fi
