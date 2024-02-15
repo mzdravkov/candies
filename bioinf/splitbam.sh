1629a1ece95c47afe677fb1ec82b8e8e5936a798
Mihail Zdravkov <mihail0zdravkov@gmail.com>
2024-02-15 13:52:59 +0100

    Add a tool for splitting a bam file into two

---
+++ bioinf/splitbam.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+
+usage="$(basename "$0") [-h] [-s n] [-f n] [-i INPUT_BAM] [-1 OUTPUT1] [-2 OUTPUT2] \n\nSplits a BAM randomly into two files.
+
+where:
+    -h  show this help text
+    -s  set the seed value (default: 42)
+    -f  fraction of input to sample for output1. Only decimal part, e.g. 5 for 50%.
+    -i  path to the inupt bam
+    -1  path to the first output bam
+    -2  path to the second output bam"
+
+seed=42
+frac=5
+
+unset -v input
+unset -v output1
+unset -v output2
+
+
+while getopts ':hsi:1:2:f::' option; do
+  case "$option" in
+    h) echo "$usage"
+       exit
+       ;;
+    s) seed=$OPTARG
+       ;;
+    i) input=$OPTARG
+       ;;
+    1) output1=$OPTARG
+       ;;
+    2) output2=$OPTARG
+       ;;
+    s) frac=$OPTARG
+       ;;
+    :) printf "missing argument for -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+   \?) printf "illegal option: -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+if [ -z "$input" ]; then
+  echo 'Missing -i' >&2
+  exit 1
+fi
+if [ -z "$output1" ]; then
+  echo 'Missing option -1' >&2
+  echo "$usage"
+  exit 1
+fi
+if [ -z "$output2" ]; then
+  echo 'Missing option -2' >&2
+  echo "$usage"
+  exit 1
+fi
+
+
+samtools view -bo $output1 -s $seed.$frac $input
+samtools view $output1 | cut -f1 > ids.tmp
+samtools view -bo split2.bam -N ^ids.tmp basecalled_pod5.bam
+rm ids.tmp
+
