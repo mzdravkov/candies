e85a2a1c84e9f745945229c4f26841236d403c18
Mihail Zdravkov <mihail0zdravkov@gmail.com>
2024-02-28 14:39:58 +0100

    Use multiple threads for splitbam

--- bioinf/splitbam.sh
+++ bioinf/splitbam.sh
@@ -7,19 +7,21 @@ where:
     -h  show this help text
     -s  set the seed value (default: 42)
     -f  fraction of input to sample for output1. Only decimal part, e.g. 5 for 50%.
+    -t  number of threads
     -i  path to the inupt bam
     -1  path to the first output bam
     -2  path to the second output bam"
 
 seed=42
 frac=5
+threads=1
 
 unset -v input
 unset -v output1
 unset -v output2
 
 
-while getopts ':hsi:1:2:f::' option; do
+while getopts ':hsit:1:2:f::' option; do
   case "$option" in
     h) echo "$usage"
        exit
@@ -27,6 +29,8 @@ while getopts ':hsi:1:2:f::' option; do
     s) seed=$OPTARG
        ;;
     i) input=$OPTARG
+       ;;
+    t) threads=$OPTARG
        ;;
     1) output1=$OPTARG
        ;;
@@ -62,8 +66,8 @@ if [ -z "$output2" ]; then
 fi
 
 
-samtools view -bo $output1 -s $seed.$frac $input
-samtools view $output1 | cut -f1 > ids.tmp
-samtools view -bo $output2 -N ^ids.tmp $input
+samtools view -t $threads -bo $output1 -s $seed.$frac $input
+samtools view -t $threads $output1 | cut -f1 > ids.tmp
+samtools view -t $threads -bo $output2 -N ^ids.tmp $input
 rm ids.tmp
 
